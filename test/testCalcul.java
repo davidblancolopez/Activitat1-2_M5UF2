927ca1a2081c4a8e717819a1176235c7b0d3eec3
davidblancolopez <daviddbl94@gmail.com>
2016-10-20 20:35:56 +0200

    Cambios

--- test/testCalcul.java
+++ test/testCalcul.java
@@ -52,11 +52,23 @@ public class testCalcul {
         assertEquals(90, Calcul.calculPreu(3));
     }
     
-    //Test de preu 1 introducimos el numero 3 y nos devolvera 90 del calculo de 3 * 30.
+    //Test de preu 2 introducimos el numero 13 y nos devolvera 380 que es el precio de matricula a partir de 12 o mas UF.
     @Test
     public final void testPreu2(){
         
         assertEquals(380, Calcul.calculPreu(13));
     }
+    
+    /*
+    
+    //Test de preu 3 introducimos una letra y nos tendria que mostrar un error
+    //indicando que introduzcamos numeros enteros.
+    @Test
+    public final void testPreu3(){
+        
+        assertEquals(noEnters, Calcul.calculPreu("a"));
+    }
+    */
+    
     
 }
