2c1b6700f096628b26eb014023caaccc347cc710
davidblancolopez <daviddbl94@gmail.com>
2016-10-20 20:06:00 +0200

    Cambios

---
+++ test/testCalcul.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ALUMNEDAM
+ */
+public class testCalcul {
+    
+    public testCalcul() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    
+
+    //Test de factorial 1 introducimos un numero que nos devolvera -1
+    //Hemos puesto 0.
+    @Test
+    public final void testFactorial() {
+        //fail ("Sin implementar");
+        
+        assertEquals(90, Calcul.calculPreu(3));
+    }
+}
