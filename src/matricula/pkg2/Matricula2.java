5d97e6f3ff04d88c44686678d9da6c493db3f859
davidblancolopez <daviddbl94@gmail.com>
2016-10-20 20:02:30 +0200

    Cambios

--- src/matricula/pkg2/Matricula2.java
+++ src/matricula/pkg2/Matricula2.java
@@ -7,19 +7,7 @@ import java.util.Scanner;
 public class Matricula2 {
 
     public static void main(String[] args) {
-    Scanner lector = new  Scanner(System.in);
-    Calcul calcul = new Calcul();
-    int num;
-    
-        do{
-        System.out.println("Introdueix el numero de UF 's en que estas matriculat: ");
-        num = lector.nextInt();
-        
-        }while(num < 0);
-    
-    int preu = calcul.calculPreu(num);
-    
-        System.out.println(preu);
+
         
     }
     
